ec3081ab3f3e3274dd12e9a1f0f26ed4aef3578b
avraham <2721avi@gmail.com>
2024-11-06 10:59:28 +0200

    Basic routing

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Claims from "./claims/Claims";
+import Policies from "./policies/Policies";
+import PolicyDetails from "./policies/PolicyDetails";
+import ClaimDetails from "./claims/ClaimDetails";
 
 export default function Page() {
   return (
-    <div>Page</div>
-  )
+    <div className="full">
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/policies" />} />
+        <Route path="claims" element={<Claims />} />
+        <Route path="claims/:id" element={<ClaimDetails />} />
+        <Route path="policies" element={<Policies />} />
+        <Route path="policies/:id" element={<PolicyDetails />} />
+      </Routes>
+    </div>
+  );
 }
